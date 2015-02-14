6c0e0613771edec2fd6533a06161048b146ff2ad
Shawn Lim <shawn@missile.io>
2015-02-14 13:51:49 +0800

    make second cuke test pass

--- common/routes/lib/config.js
+++ common/routes/lib/config.js
@@ -1,3 +1,11 @@
 Router.configure({
   layoutTemplate: 'MainLayout'
 });
+
+AccountsTemplates.configureRoute('ensureSignedIn', {
+  template: 'restricted'
+});
+
+Router.onBeforeAction(AccountsTemplates.ensureSignedIn, {
+  only: ['create']
+});
