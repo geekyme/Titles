88108451cf03edc4c9510a26cc308dba03901074
Shawn Lim <shawn@missile.io>
2015-02-15 13:19:30 +0800

    adding unit test to guard allow rules

--- tests/jasmine/server/unit/titles-security.spec.js
+++ tests/jasmine/server/unit/titles-security.spec.js
@@ -1,9 +1,20 @@
 describe("Titles security rules", function() {
+  var allow;
+
+  beforeEach(function(){
+    allow = Titles._velocityAllow;
+  });
+
   it("should allow inserts when user is logged in", function() {
-    
+    // when user is logged out, userId arg is the _id of a user doc
+    var response = allow.insert('1212adsa2131', {});
+    expect(response).toBe(true);
   });
 
   it("should not allow inserts when user is logged out", function() {
+    // when user is logged out, userId arg is null
+    var response = allow.insert(null, {});
+    expect(response).toBe(false);
     
   });
 });
